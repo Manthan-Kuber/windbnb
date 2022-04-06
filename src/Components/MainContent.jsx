ed258a12d5d1c1542754277fcfb798b15b8c8c79
Manthan Kuber <manthankuber873@gmail.com>
2022-04-06 19:38:33 +0530

    feat:made main content responsive

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 import StaysData from "../stays.json";
+import { FaStar } from "react-icons/fa";
 
 const MainContent = () => {
   return (
     <>
       <TitleContainer>
         <h1>Stays in Finland</h1>
-        <h5>12+ stays</h5>
+        <h5>{StaysData.length}+ Stays</h5>
       </TitleContainer>
       <CardsContainer>
         {StaysData.map((stay, index) => (
           <Card>
-            <img src={stay.photo} alt={`image no. ${index}`} />
+            <img src={stay.photo} alt={`Card no. ${index}`} />
+            <InfoContainer>
+              <HostWrapper>
+                {stay.superHost && <h2>Super Host</h2>}
+                <p>
+                  {stay.type} .{stay.beds} beds
+                </p>
+              </HostWrapper>
+              <RatingWrapper>
+                <FaStar className="star-icon" />
+                <p>{stay.rating}</p>
+              </RatingWrapper>
+            </InfoContainer>
+            <Title>{stay.title}</Title>
           </Card>
         ))}
       </CardsContainer>
@@ -40,17 +54,76 @@ const TitleContainer = styled.div`
 
 const CardsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  
-  gap: 1rem;
+  grid-row-gap: 3.2rem;
+  @media (min-width: 80em) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-column-gap: 3.2rem;
+    grid-row-gap: 5rem;
+  }
 `;
 
 const Card = styled.article`
-  background-color: red;
+  display: flex;
+  flex-direction: column;
+  max-width: 60rem;
+  max-height: 60rem;
+  margin-inline: auto;
   img {
     width: 100%;
+    height: 100%;
     object-fit: cover;
-    height: auto;
     border-radius: 24px;
   }
+  @media (min-width: 80em) {
+    display: grid;
+    grid-template-rows: 30rem;
+    max-width: revert;
+    max-height: revert;
+    margin-inline: revert;
+  }
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+`;
+
+const HostWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  h2 {
+    text-transform: uppercase;
+    font: 700 clamp(1rem, 0.5vw + 0.5rem, 1.4rem) "Montserrat", sans-serif;
+    color: #4f4f4f;
+    border: 1px solid #4f4f4f;
+    padding: 0.7rem 1rem;
+    border-radius: 12px;
+    margin-right: 1rem;
+  }
+  p {
+    font: 500 clamp(1.2rem, 0.5vw + 0.5rem, 1.4rem) "Montserrat", sans-serif;
+    color: #828282;
+  }
+`;
+
+const RatingWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  .star-icon {
+    color: var(--clr-primary);
+  }
+  p {
+    font: 500 clamp(1.2rem, 0.5vw + 0.5rem, 1.4rem) "Montserrat", sans-serif;
+    color: #828282;
+    white-space: nowrap;
+  }
+`;
+
+const Title = styled.h1`
+  color: #333;
+  font: 600 clamp(1.4rem, 0.5vw + 0.5rem, 1.6rem) "Montserrat", sans-serif;
+  margin-top: 1.2rem;
 `;
